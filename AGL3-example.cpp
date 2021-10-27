25885c135f7ae7bc8e409bd866c28957903e3e6c
Paweł Szmergała <pawelszme@gmail.com>
2021-10-27 13:24:29 +0200

    Remove former assignment code from `AGL3-example.cpp`

--- AGL3-example.cpp
+++ AGL3-example.cpp
@@ -11,139 +11,6 @@
 
 #include "AGL3Window.hpp"
 #include "AGL3Drawable.hpp"
-#include "CirclePolygon.hpp"
-
-// ==========================================================================
-// Drawable object: no-data only: vertex/fragment programs
-// ==========================================================================
-class MyTri : public AGLDrawable {
-public:
-   MyTri() : AGLDrawable(0) {
-      setShaders();
-   }
-   void setShaders() {
-      compileShaders(R"END(
-
-         #version 330
-         out vec4 vcolor;
-         out vec2 pos;
-
-         void main(void) {
-            const vec2 vertices[3] = vec2[3](vec2( 0.9, -0.9),
-                                             vec2(-0.9, -0.9),
-                                             vec2( 0.9,  0.9));
-            const vec4 colors[]    = vec4[3](vec4(1.0, 0.0, 0.0, 1.0),
-                                             vec4(0.0, 1.0, 0.0, 1.0),
-                                             vec4(0.0, 0.0, 1.0, 1.0));
-
-            vcolor      = colors[gl_VertexID];
-            pos = vertices[gl_VertexID];
-            gl_Position = vec4(vertices[gl_VertexID], 0.5, 1.0);
-         }
-
-      )END", R"END(
-
-         #version 330 
-         in  vec4 vcolor;
-         in vec2 pos;
-
-         out vec4 color;
-
-         void main(void) {
-            if (distance(pos, vec2(0.0, 0.0)) <= 0.25)
-                color = vec4(1.0, 1.0, 1.0, 1.0);
-            else
-                color = vcolor;
-         } 
-
-      )END");
-   }
-   void draw() override {
-      bindProgram();
-      glDrawArrays(GL_TRIANGLES, 0, 3);
-   }
-};
-
-
-// ==========================================================================
-// Drawable object with some data in buffer and vertex/fragment programs
-// ==========================================================================
-class MyCross : public AGLDrawable {
-public:
-    GLfloat armLength;
-
-   explicit MyCross(GLfloat armLength) : AGLDrawable(0) {
-       if (armLength <= 0)
-           throw std::invalid_argument("armLength must be non-negative");
-       this->armLength = armLength;
-      setShaders();
-      setBuffers();
-   }
-   void setShaders() {
-      compileShaders(R"END(
-
-         #version 330
-         #extension GL_ARB_explicit_uniform_location : require
-         #extension GL_ARB_shading_language_420pack : require
-         layout(location = 0) in vec2 pos;
-         layout(location = 0) uniform float scale;
-         layout(location = 1) uniform vec2  center;
-         out vec4 vtex;
-
-         void main(void) {
-            vec2 p = (pos * scale + center);
-            gl_Position = vec4(p, 0.0, 1.0);
-         }
-
-      )END", R"END(
-
-         #version 330
-         #extension GL_ARB_explicit_uniform_location : require
-         layout(location = 3) uniform vec3  cross_color;
-         out vec4 color;
-
-         void main(void) {
-            color = vec4(cross_color,1.0);
-         }
-
-      )END");
-   }
-   void setBuffers() {
-      bindBuffers();
-      GLfloat vert[4][2] = {  // Cross lines
-         { -1,  0  },
-         {  1,  0  },
-         {  0, -1  },
-         {  0,  1  }
-      };
-
-      glBufferData(GL_ARRAY_BUFFER, 4*2*sizeof(float), vert, GL_STATIC_DRAW );
-      glEnableVertexAttribArray(0);
-      glVertexAttribPointer(
-         0,                 // attribute 0, must match the layout in the shader.
-         2,                  // size
-         GL_FLOAT,           // type
-         GL_FALSE,           // normalized?
-         0,//24,             // stride
-         (void*)0            // array buffer offset
-      );
-   }
-   void draw() override {
-      bindProgram();
-      bindBuffers();
-      glUniform1f(0, armLength);  // scale  in vertex shader
-      glUniform2f(1, pos.x, pos.y);  // center in vertex shader
-      glUniform3f(3, cross_color[0],cross_color[1],cross_color[2]);
-
-      glDrawArrays(GL_LINES, 0, 4);
-   }
-   void setColor(float r, float g, float b){
-      cross_color[0]=r;cross_color[1]=g;cross_color[2]=b;
-   }
- private:
-   GLfloat cross_color[3] = { 0.0, 1.0, 0.0 };
-};
-
 
 // ==========================================================================
 // Window Main Loop Inits ...................................................
@@ -157,7 +24,6 @@ public:
     void MainLoop() override;
 };
 
-
 // ==========================================================================
 void MyWin::KeyCB(int key, int scancode, int action, int mods) {
     AGLWindow::KeyCB(key,scancode, action, mods); // f-key full screen switch
@@ -170,89 +36,43 @@ void MyWin::KeyCB(int key, int scancode, int action, int mods) {
 }
 
 // ==========================================================================
-bool isCollision(const MyCross& cross, const CirclePolygon& circle) {
-    GLfloat hx1 = cross.pos.x - cross.armLength, hx2 = cross.pos.x + cross.armLength;
-    GLfloat vy1 = cross.pos.y + cross.armLength, vy2 = cross.pos.y - cross.armLength;
-
-    //// check vertical cross bar collision
-    GLfloat vDist = std::abs(cross.pos.y - circle.pos.y);
-    // circle center between horizontal ends of the cross
-    if (vDist <= circle.radius && hx1 <= circle.pos.x && circle.pos.x <= hx2)
-        return true;
-    // circle center on the left side of the cross
-    if (glm::distance(glm::vec2(hx1, cross.pos.y), glm::vec2(circle.pos.x, circle.pos.y)) <= circle.radius)
-        return true;
-    // circle center on the right side of the cross
-    if (glm::distance(glm::vec2(hx2, cross.pos.y), glm::vec2(circle.pos.x, circle.pos.y)) <= circle.radius)
-        return true;
-
-    //// check horizontal cross bar collision
-    GLfloat hDist = std::abs(cross.pos.x - circle.pos.x);
-    // circle center between vertical ends of the cross
-    if (hDist <= circle.radius && vy1 <= circle.pos.y && circle.pos.y <= vy2)
-        return true;
-    // circle center above the cross
-    if (glm::distance(glm::vec2(cross.pos.x, vy1), glm::vec2(circle.pos.x, circle.pos.y)) <= circle.radius)
-        return true;
-    // circle center under the cross
-    if (glm::distance(glm::vec2(cross.pos.x, vy2), glm::vec2(circle.pos.x, circle.pos.y)) <= circle.radius)
-        return true;
-
-    return false;
-}
-
-// ==========================================================================
 void MyWin::MainLoop() {
-   ViewportOne(0,0,wd,ht);
+    ViewportOne(0,0,wd,ht);
 
-   MyCross cross(1.0/16);
-   cross.pos.x = 0.0; cross.pos.y = 0.5;
-   MyTri   trian;
-   CirclePolygon circlePoly(64, 1.0/16);
-   circlePoly.pos.x = -0.5; circlePoly.pos.y = 0.5;
+    do {
+        glClear( GL_COLOR_BUFFER_BIT );
 
-   do {
-      glClear( GL_COLOR_BUFFER_BIT );
-   
-      AGLErrors("main-loopbegin");
-      // =====================================================        Drawing
-      trian.draw();
-      cross.draw();
-      circlePoly.draw();
-      AGLErrors("main-afterdraw");
+        AGLErrors("main-loopbegin");
+        // =====================================================        Drawing
+        // TODO draw scene elements
+        AGLErrors("main-afterdraw");
 
-      glfwSwapBuffers(win()); // =============================   Swap buffers
-      glfwPollEvents();
-      //glfwWaitEvents();   
+        glfwSwapBuffers(win()); // =============================   Swap buffers
+        glfwPollEvents();
+        //glfwWaitEvents();
 
-      // triangle filling
-      if (glfwGetKey(win(), GLFW_KEY_DOWN ) == GLFW_PRESS) {
-         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-      } else if (glfwGetKey(win(), GLFW_KEY_UP ) == GLFW_PRESS) {
-         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-     // cross moving
-      } else if (glfwGetKey(win(), GLFW_KEY_RIGHT ) == GLFW_PRESS) {
-         cross.pos.x += 0.01;
-      } else if (glfwGetKey(win(), GLFW_KEY_LEFT ) == GLFW_PRESS) {
-         cross.pos.x -= 0.01;
-     // circle moving
-      } else if (glfwGetKey(win(), GLFW_KEY_W) == GLFW_PRESS) {
-         circlePoly.pos.y += 0.01;
-      } else if (glfwGetKey(win(), GLFW_KEY_S) == GLFW_PRESS) {
-         circlePoly.pos.y -= 0.01;
-      } else if (glfwGetKey(win(), GLFW_KEY_A) == GLFW_PRESS) {
-         circlePoly.pos.x -= 0.01;
-      } else if (glfwGetKey(win(), GLFW_KEY_D) == GLFW_PRESS) {
-         circlePoly.pos.x += 0.01;
-      }
-   } while( glfwGetKey(win(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-            glfwWindowShouldClose(win()) == 0 &&
-            !isCollision(cross, circlePoly));
+        // polygon filling
+        if (glfwGetKey(win(), GLFW_KEY_DOWN ) == GLFW_PRESS) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        } else if (glfwGetKey(win(), GLFW_KEY_UP ) == GLFW_PRESS) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        // player moving
+        } else if (glfwGetKey(win(), GLFW_KEY_RIGHT ) == GLFW_PRESS) {
+            // TODO
+        } else if (glfwGetKey(win(), GLFW_KEY_LEFT ) == GLFW_PRESS) {
+            // TODO
+        } else if (glfwGetKey(win(), GLFW_KEY_UP) == GLFW_PRESS) {
+            // TODO
+        } else if (glfwGetKey(win(), GLFW_KEY_DOWN) == GLFW_PRESS) {
+            // TODO
+        }
+    } while(glfwGetKey(win(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+            glfwWindowShouldClose(win()) == 0);
 }
 
 int main(int argc, char *argv[]) {
-   MyWin win;
-   win.Init(800,600,"AGL3 example",0,33);
-   win.MainLoop();
-   return 0;
+    MyWin win;
+    win.Init(800,600,"AGL3 example",0,33);
+    win.MainLoop();
+    return 0;
 }
