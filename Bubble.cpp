46184e86c2546cccabb8fe35e5911f999b14720d
Paweł Szmergała <pawelszme@gmail.com>
2021-12-04 15:45:50 +0100

    Fix random bubble color generation

--- Bubble.cpp
+++ Bubble.cpp
@@ -1,9 +1,10 @@
 #include "Bubble.hpp"
 
+#include <cstdlib>
 #include <glm/gtx/color_space.hpp>
 
 Bubble::Bubble(float scale) : scale(scale) {
-    float hue = (float)rand() / (float)RAND_MAX;
+    float hue = (float)rand() / (float)RAND_MAX * 360.f;
     col = glm::rgbColor(glm::vec3(hue, 1.f, 1.f));
 }
 
