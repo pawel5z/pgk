7fdf9272d27a7f3ec1d0b9ea1b0b63ca591471ea
Paweł Szmergała <pawelszme@gmail.com>
2021-12-08 23:01:05 +0100

    Change place of aquarium lighting computations to fragment shader

--- cubeFS.glsl
+++ cubeFS.glsl
@@ -1,10 +1,24 @@
 #version 330
 #extension GL_ARB_explicit_uniform_location : require
 
-in vec3 fragmentColor;
+layout(location = 3) uniform vec3 dlColor;
 
-out vec4 outputFragColor;
+in vec3 normalWorldspace;
+in vec3 lightDirWorldspace;
+in vec3 eyeDirWorldspace;
+
+out vec4 color;
+
+const vec3 diffuseCol = vec3(.094f, .623f, .949f);
+const vec3 ambientCol = vec3(1.f) * .1f;
 
 void main(void) {
-    outputFragColor = vec4(fragmentColor, 1.f);
+    vec3 n = normalize(normalWorldspace);
+    vec3 l = normalize(lightDirWorldspace);
+    vec3 e = normalize(eyeDirWorldspace);
+    vec3 r = reflect(-l, n);
+    vec3 fragmentColor = diffuseCol * dlColor * clamp(dot(n, l), 0.f, 1.f) // diffuse component
+                       + vec3(.5f, .5f, .5f) * pow(clamp(dot(e, r), 0.f, 1.f), 16.f) * float(dot(l, n) > 0.f) // specular component
+                       + ambientCol;
+    color = vec4(fragmentColor, 1.f);
 }
--- cubeVS.glsl
+++ cubeVS.glsl
@@ -5,26 +5,19 @@
 layout(location = 0) uniform mat4 m;
 layout(location = 1) uniform mat4 vp;
 layout(location = 2) uniform vec3 dlDirWorldspace;
-layout(location = 3) uniform vec3 dlColor;
 layout(location = 4) uniform vec3 cameraPosWorldspace;
 
 layout(location = 0) in vec3 vertexPosModelspace;
 
-out vec3 fragmentColor;
-
-const vec3 ambientCol = vec3(1.f) * .1f;
+out vec3 normalWorldspace;
+out vec3 lightDirWorldspace;
+out vec3 eyeDirWorldspace;
 
 void main(void) {
     vec3 vertexPosWorldspace = vec3(m * vec4(vertexPosModelspace, 1.f));
     gl_Position = vp * vec4(vertexPosWorldspace, 1.f);
 
-    const vec3 diffuseCol = vec3(.094f, .623f, .949f);
-    vec3 n = normalize(transpose(inverse(mat3(m))) * normalize(vertexPosModelspace));
-    vec3 l = normalize(-dlDirWorldspace);
-    vec3 e = normalize(cameraPosWorldspace - vertexPosWorldspace);
-    vec3 r = reflect(-l, n);
-    fragmentColor = diffuseCol * dlColor * clamp(dot(n, l), 0.f, 1.f) // diffuse component
-                  + vec3(.5f, .5f, .5f) * pow(clamp(dot(e, r), 0.f, 1.f), 7.f) * float(dot(l, n) > 0.f) // specular component
-                  + ambientCol
-    ;
+    normalWorldspace = normalize(transpose(inverse(mat3(m))) * normalize(vertexPosModelspace));
+    lightDirWorldspace = normalize(-dlDirWorldspace);
+    eyeDirWorldspace = normalize(cameraPosWorldspace - vertexPosWorldspace);
 }
