5ff2c3b836a540b201e2b9f5bdc2a73704732fc6
Paweł Szmergała <pawelszme@gmail.com>
2021-12-08 22:24:00 +0100

    Change place of player sphere lighting computations to fragment shader

--- sphereFS.glsl
+++ sphereFS.glsl
@@ -1,10 +1,24 @@
 #version 330
 #extension GL_ARB_explicit_uniform_location : require
 
-in vec3 fragmentColor;
+layout(location = 3) uniform vec3 dlColor;
+
+in vec3 diffuseCol;
+in vec3 normalWorldspace;
+in vec3 lightDirWorldspace;
+in vec3 eyeDirWorldspace;
 
 out vec4 color;
 
+const vec3 ambientCol = vec3(1.f) * .1f;
+
 void main(void) {
+    vec3 n = normalize(normalWorldspace);
+    vec3 l = normalize(lightDirWorldspace);
+    vec3 e = normalize(eyeDirWorldspace);
+    vec3 r = reflect(-l, n);
+    vec3 fragmentColor = diffuseCol * dlColor * clamp(dot(n, l), 0.f, 1.f); // diffuse component
+                       + vec3(.5f, .5f, .5f) * pow(clamp(dot(e, r), 0.f, 1.f), 7.f) * float(dot(l, n) > 0.f) // specular component
+                       + ambientCol;
     color = vec4(fragmentColor, 1.0);
 }
