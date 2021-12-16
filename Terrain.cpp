634f0ded1e99c86bfe01a06510a1944b0f184e7b
Paweł Szmergała <pawelszme@gmail.com>
2021-12-16 23:04:08 +0100

    Add drawing support for `Terrain`

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,18 +1,76 @@
 #include "Terrain.hpp"
+#include "utils.hpp"
 
-Terrain::Terrain() {
+#include <filesystem>
+#include <stdexcept>
+
+Terrain::Terrain(const std::string& dirPath) {
+    try {
+        for (const auto &dirEntry : std::filesystem::directory_iterator(dirPath)) {
+            if (!dirEntry.is_regular_file())
+                continue;
+            try {
+                areaFrags.emplace_back(dirEntry.path());
+            } catch (std::invalid_argument &e) {
+                fprintf(stderr, "%s\n", e.what());
+                throw std::invalid_argument("incorrect file name in specified path");
+            }
+        }
+    } catch (std::exception &e) {
+        fprintf(stderr, "%s\n", e.what());
+        throw std::logic_error("error while reading " + dirPath + " directory");
+    }
+
+    float minLo = std::numeric_limits<float>::max(), maxLo = std::numeric_limits<float>::min(),
+          minLa = std::numeric_limits<float>::max(), maxLa = std::numeric_limits<float>::min();
+    for (auto &area : areaFrags) {
+        minLo = std::min(minLo, (float)area.getLeftLo());
+        maxLo = std::max(maxLo, (float)area.getRightLo());
+        minLa = std::min(minLa, (float)area.getLowLa());
+        maxLa = std::max(maxLa, (float)area.getHighLa());
+    }
+    midLo = (minLo + maxLo) / 2.f;
+    midLa = (minLa + maxLa) / 2.f;
+
     GLushort step = 1;
     for (GLushort lod = 0; lod <= maxLOD; lod++) {
+        indices.emplace_back();
         for (GLushort row = 0; row < Terrain::elementsCnt - step; row += step) {
             for (GLushort col = 0; col < Terrain::elementsCnt; col += step) {
-                indices[lod].push_back(row * Terrain::elementsCnt + col);
-                indices[lod].push_back((row + step) * Terrain::elementsCnt + col);
+                indices.at(lod).push_back(row * Terrain::elementsCnt + col);
+                indices.at(lod).push_back((row + step) * Terrain::elementsCnt + col);
             }
         }
         step *= 2;
     }
+
+    compileShadersFromFile("areaMapVS.glsl", "areaFS.glsl");
+    glEnableVertexAttribArray(0);
+}
+
+float Terrain::getMidLo() const {
+    return midLo;
+}
+
+float Terrain::getMidLa() const {
+    return midLa;
 }
 
 void Terrain::draw(Camera camera) {
+    throw std::logic_error("not implemented");
+}
 
+void Terrain::draw(Camera camera, int lod) {
+    bind();
+    // eboId already bound
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (int)(indices[lod].size() * sizeof(GLushort)), indices[lod].data(), GL_STATIC_DRAW);
+    glUniformMatrix4fv(0, 1, false, &(camera.getPVMat() * getModelMat())[0][0]);
+    for (auto &area : areaFrags) {
+        glUniform2f(1, area.getLeftLo(), area.getLowLa());
+        int verticesCntInStrip = ((Terrain::elementsCnt - 1) / fastPow(2, lod) + 1) * 2;
+        area.prepareForDrawing();
+        for (size_t i = 0; i < indices.at(lod).size(); i += verticesCntInStrip) {
+            glDrawElements(GL_TRIANGLE_STRIP, verticesCntInStrip, GL_UNSIGNED_SHORT, (void *)i);
+        }
+    }
 }
--- Terrain.hpp
+++ Terrain.hpp
@@ -2,22 +2,28 @@
 #define TERRAIN_HPP
 
 #include "AGL3Drawable.hpp"
+#include "AreaFragment.hpp"
 
 #include <vector>
+#include <string>
 #include <glm/glm.hpp>
 
 static GLubyte maxLOD = 3;
 
 class Terrain : AGLDrawable {
 public:
-    static const GLushort elementsCnt = 1021;
+    static const GLushort elementsCnt = 1201;
 
-    Terrain();
+    explicit Terrain(const std::string& dirPath);
+    float getMidLo() const;
+    float getMidLa() const;
+    void draw(Camera camera) override;
+    void draw(Camera camera, int lod);
 
 private:
+    std::vector<AreaFragment> areaFrags;
     std::vector<std::vector<GLushort>> indices;
-
-    void draw(Camera camera) override;
+    float midLo, midLa;
 };
 
 #endif //TERRAIN_HPP
