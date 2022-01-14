ec8865710cfea575dd106823929adad7e78c3044
Paweł Szmergała <pawelszme@gmail.com>
2022-01-14 15:17:28 +0100

    Draw sphere using triangle strips

--- Sphere.cpp
+++ Sphere.cpp
@@ -9,7 +9,7 @@
 void Sphere::draw(Camera camera) {
     bind();
     glUniformMatrix4fv(0, 1, false, &(camera.getPVMat() * getModelMat())[0][0]);
-    glDrawElements(GL_TRIANGLES, (int)indicesCnt, GL_UNSIGNED_SHORT, nullptr);
+    glDrawElements(GL_TRIANGLE_STRIP, (int)indicesCnt, GL_UNSIGNED_SHORT, nullptr);
     drawnTrianglesCnt += indicesCnt / 3;
 }
 
@@ -49,14 +49,13 @@ void Sphere::initWithPolarCoords(int stacks, int sectors, std::vector<glm::vec3>
         throw std::invalid_argument("Incorrect stacks number: " + std::to_string(stacks) + "\n");
     if (sectors < 3 || 360 < sectors)
         throw std::invalid_argument("Incorrect sectors number: " + std::to_string(sectors) + "\n");
-/*
- * lo         lo+1
+/* lo         lo+1
  *
- * v3 ------- v2    la+1
+ * v1 ------- v3   la+1
  *  |         |
  *  |         |
- * v0 ------- v1    la
- * */
+ * v0 ------- v2   la
+ */
     auto hash = [](const glm::vec3 &v) { return std::hash<size_t>{}(std::hash<float>{}(v.x) * std::hash<float>{}(v.y) * std::hash<float>{}(v.z)); };
     auto comp = [](const glm::vec3 &v, const glm::vec3 &u) { return v.x == u.x & v.y == u.y && v.z == u.z; };
     std::unordered_map<glm::vec3, GLushort, decltype(hash), decltype(comp)> existing(10, hash, comp);
@@ -64,9 +63,9 @@ void Sphere::initWithPolarCoords(int stacks, int sectors, std::vector<glm::vec3>
         for (int lo = 0; lo < sectors; lo++) {
             std::vector<glm::vec3> v {
                     pointOnSphere(latitudeAng(la, stacks), longitudeAng(lo, sectors), .5f),
+                    pointOnSphere(latitudeAng(la + 1, stacks), longitudeAng(lo, sectors), .5f),
                     pointOnSphere(latitudeAng(la, stacks), longitudeAng(lo + 1, sectors), .5f),
-                    pointOnSphere(latitudeAng(la + 1, stacks), longitudeAng(lo + 1, sectors), .5f),
-                    pointOnSphere(latitudeAng(la + 1, stacks), longitudeAng(lo, sectors), .5f)
+                    pointOnSphere(latitudeAng(la + 1, stacks), longitudeAng(lo + 1, sectors), .5f)
             };
             for (auto &candidate : v)
                 if (!existing.count(candidate)) {
@@ -79,8 +78,8 @@ void Sphere::initWithPolarCoords(int stacks, int sectors, std::vector<glm::vec3>
                         throw std::logic_error("Too many vertices.");
                     existing[candidate] = vertices.size() - 1;
                 }
-            indices.insert(indices.end(), {existing[v[0]], existing[v[1]], existing[v[3]]});
-            indices.insert(indices.end(), {existing[v[1]], existing[v[2]], existing[v[3]]});
+            indices.insert(indices.end(), {existing[v[0]], existing[v[1]], existing[v[2]], existing[v[3]]});
         }
+        indices.push_back((GLushort)primitiveRestartIndex);
     }
 }
